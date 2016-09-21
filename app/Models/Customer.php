19a496aab9cf61fcc3125d8a1826efec2a0d1115
gnailiylin <gnailiylinwiz@gmail.com>
2016-09-21 22:02:38 +0800

    fix some bug

---
+++ app/Models/Customer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gnailiylin
+ * Date: 16/9/5
+ * Time: 下午4:02
+ */
+
+namespace App\Models;
+
+
+class Customer
+{
+
+}
---
+++ app/Models/Supplier.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gnailiylin
+ * Date: 16/9/5
+ * Time: 下午4:00
+ */
+
+namespace App\Models;
+
+
+class Supplier
+{
+
+}
